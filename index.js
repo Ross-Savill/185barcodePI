3c0fad1e1decdc60906d71e05318a70afd067b07
Steve <admin@cryptostat.org>
2019-01-30 17:50:25 +1100

    changed from server to client

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const port = 8080;
+let connected = false;
+app.listen(port, () => {
+  console.log(`[!] Listening on port: ${port}`)
+})
 
 app.get('/', function(req, res,next) {
     res.status(200).send('<h3>Status: <span style="color:green">Up</span></h3>')
 });
 
+const socket = require('socket.io-client')('http://localhost:8000');
+socket.on('connect', function(){
+  console.log(`[!] Ready`);
+});
+
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function (barcode) {
-  barcode = barcode.trim()
-  io.emit('barcode', barcode);
+  barcode = barcode.trim();
+  socket.emit('barcode', barcode);
 })
-
-server.listen(5200);
-
-io.on('connection', function(client) {
-    console.log('Client connected');
-});
